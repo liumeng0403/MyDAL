3a6669cf68406943cab2ae910ffebbf06395e077
liumeng <liumeng0403@163.com>
2018-09-03 21:14:21 +0800

    [add] 修正时间截断到 s 的问题

--- EasyDAL.Exchange/Core/Query/QueryFilter.cs
+++ EasyDAL.Exchange/Core/Query/QueryFilter.cs
@@ -120,6 +120,15 @@ namespace EasyDAL.Exchange.Core.Query
         {
             return await QueryPagingListAsyncHandle<M, M>(pageIndex, pageSize, SqlTypeEnum.QueryPagingListAsync);
         }
+        ///// <summary>
+        ///// 单表分页查询
+        ///// </summary>
+        ///// <param name="pageIndex">页码</param>
+        ///// <param name="pageSize">每页条数</param>
+        //public async Task<PagingList<M>> QueryPagingListAsync()
+        //{
+        //    return await QueryPagingListAsyncHandle<M, M>(pageIndex, pageSize, SqlTypeEnum.QueryPagingListAsync);
+        //}
         /// <summary>
         /// 单表分页查询
         /// </summary>
@@ -130,6 +139,16 @@ namespace EasyDAL.Exchange.Core.Query
         {
             return await QueryPagingListAsyncHandle<M, VM>(pageIndex, pageSize, SqlTypeEnum.QueryPagingListAsync);
         }
+        ///// <summary>
+        ///// 单表分页查询
+        ///// </summary>
+        ///// <typeparam name="VM">ViewModel</typeparam>
+        ///// <param name="pageIndex">页码</param>
+        ///// <param name="pageSize">每页条数</param>
+        //public async Task<PagingList<VM>> QueryPagingListAsync<VM>()
+        //{
+        //    return await QueryPagingListAsyncHandle<M, VM>(pageIndex, pageSize, SqlTypeEnum.QueryPagingListAsync);
+        //}
 
     }
 }
---
+++ EasyDAL.Exchange/Extensions/ObjectMethodExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyDAL.Exchange.Extensions
+{
+    public static class ObjectMethodExtensions
+    {
+
+        /// <summary>
+        ///  obj --> datetime -->str
+        /// </summary>
+        public static string ToDatetimeStr(this object obj)
+        {
+            var result = string.Empty;
+            try
+            {
+                result = Convert.ToDateTime(obj).ToString("yyyy-MM-dd HH:mm:ss.ffffff");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("string ToDatetimeStr(this object obj)", ex);
+            }
+            return result;
+        }
+
+    }
+}
