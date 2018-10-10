592988dafb5a10e3d044b97412b3ec715f7661e3
liumeng0403 <liumeng0403@163.com>
2018-10-10 13:29:44 +0800

    [add]  bug-fix

--- EasyDAL.Exchange/Impls/ExistImpl.cs
+++ EasyDAL.Exchange/Impls/ExistImpl.cs
@@ -1,6 +1,7 @@
 ﻿using MyDAL.Common;
 using MyDAL.Core;
 using MyDAL.Enums;
+using MyDAL.ExpressionX;
 using MyDAL.Helper;
 using MyDAL.Interfaces;
 using System.Threading.Tasks;
@@ -17,6 +18,8 @@ namespace MyDAL.Impls
 
         public async Task<bool> ExistAsync()
         {
+            DC.AddConditions(DicHandle.ConditionCountHandle(CrudTypeEnum.Query, "*"));
+            DC.IP.ConvertDic();
             var count = await SqlHelper.ExecuteScalarAsync<long>(
                 DC.Conn,
                 DC.SqlProvider.GetSQL<M>(UiMethodEnum.ExistAsync)[0],
