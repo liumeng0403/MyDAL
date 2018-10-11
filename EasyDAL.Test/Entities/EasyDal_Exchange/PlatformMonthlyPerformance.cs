b2a86f9095eb32c49beaaae03a636c7fa62f29ee
liumeng0403 <liumeng0403@163.com>
2018-10-11 12:47:17 +0800

    [add] bug case

---
+++ EasyDAL.Test/Entities/EasyDal_Exchange/PlatformMonthlyPerformance.cs
@@ -0,0 +1,30 @@
+﻿using MyDAL.Test.Enums;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyDAL.Test.Entities.EasyDal_Exchange
+{
+
+    /*
+     * CREATE TABLE `platformmonthlyperformance` (
+     * `Id` char(36) NOT NULL,
+     * `CreatedOn` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+     * `Year` int(11) NOT NULL,
+     * `Month` int(11) NOT NULL COMMENT '月份',
+     * `PerformanceAmount` decimal(65,30) NOT NULL COMMENT '金额',
+     * PRIMARY KEY (`Id`)
+     * ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+     */
+    [Table("platformmonthlyperformance")]
+    public class PlatformMonthlyPerformance 
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        public int Year { get; set; }
+        
+        public Month Month { get; set; }
+
+        public decimal PerformanceAmount { get; set; }
+    }
+}
