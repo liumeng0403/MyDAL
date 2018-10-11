a277764a09bb82e8758703f1ddb548bdc232fd91
liumeng0403 <liumeng0403@163.com>
2018-10-11 12:47:17 +0800

    [add] bug case

---
+++ EasyDAL.Test/Enums/Month.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MyDAL.Test.Enums
+{
+    public enum Month
+    {
+        [Display(Name = "一月")] January = 1,
+        [Display(Name = "二月")] February = 2,
+        [Display(Name = "三月")] March = 3,
+        [Display(Name = "四月")] April = 4,
+        [Display(Name = "五月")] May = 5,
+        [Display(Name = "六月")] June = 6,
+        [Display(Name = "七月")] July = 7,
+        [Display(Name = "八月")] August = 8,
+        [Display(Name = "九月")] September = 9,
+        [Display(Name = "十月")] October = 10, // 0x0000000A
+        [Display(Name = "十一月")] November = 11, // 0x0000000B
+        [Display(Name = "十二月")] December = 12, // 0x0000000C
+    }
+}
--- MyDAL.Test.Func/04-ExistTest.cs
+++ MyDAL.Test.Func/04-ExistTest.cs
@@ -1,5 +1,7 @@
 using MyDAL.Test.Entities.EasyDal_Exchange;
+using MyDAL.Test.Enums;
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Xunit;
 using Yunyong.DataExchange;
@@ -8,11 +10,21 @@ namespace MyDAL.Test.Func
 {
     public class _04_ExistTest : TestBase
     {
+        public class ApplyStockholderAwardAccountingVM
+        {
+            [Display(Name = "年")]
+            public int Year { get; set; }
+
+            [Display(Name = "月")]
+            public Month Month { get; set; }
+        }
 
         // 查询 是否存在
         [Fact]
         public async Task ExistAsyncTest()
         {
+            /*****************************************************************************************/
+
             var xx1 = "";
 
             var res1 = await Conn
@@ -20,7 +32,24 @@ namespace MyDAL.Test.Func
                 .Where(it => it.Id == Guid.Parse("000c1569-a6f7-4140-89a7-0165443b5a4b"))
                 .ExistAsync();
 
-            var tuple = (XDebug.SQL, XDebug.Parameters);
+            var tuple1 = (XDebug.SQL, XDebug.Parameters);
+
+            /*****************************************************************************************/
+
+            var xx2 = "";
+
+            var vm = new ApplyStockholderAwardAccountingVM();
+            vm.Year = 2018;
+            vm.Month = Month.October;
+            var res2 = await Conn
+                .Selecter<PlatformMonthlyPerformance>()
+                .Where(it => it.Year==vm.Year)
+                .And(it=>it.Month==vm.Month)
+                .ExistAsync();
+
+            var tuple2 = (XDebug.SQL, XDebug.Parameters);
+
+            /*****************************************************************************************/
 
             var xx = "";
         }
