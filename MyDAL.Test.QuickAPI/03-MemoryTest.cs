6afc955ed4cd8e727ea0e23727c8e348f9854e91
liumeng0403 <liumeng0403@163.com>
2018-11-02 18:42:52 +0800

    [add] 内存占用 test case

---
+++ MyDAL.Test.QuickAPI/03-MemoryTest.cs
@@ -0,0 +1,30 @@
+﻿using MyDAL.Test.Entities.EasyDal_Exchange;
+using MyDAL.Test.Enums;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyDAL.Test.QuickAPI
+{
+    public class _03_MemoryTest:TestBase
+    {
+        [Fact]
+        public async Task test()
+        {
+            var xx = "";
+
+            for(var i=0;i<500;i++)
+            {
+                var name = "张";
+                var res = await Conn
+                    .Selecter<Agent>()
+                    .Where(it => it.Name.Contains($"{name}%") && it.CreatedOn > WhereTest.CreatedOn || it.AgentLevel == AgentLevel.DistiAgent)
+                    .QueryListAsync();
+                Assert.True(res.Count == 2506);
+                Thread.Sleep(10);
+            }
+
+            var yy = "";
+        }
+    }
+}
