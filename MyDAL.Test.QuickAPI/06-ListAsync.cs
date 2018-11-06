80b853c5303197e47f3bf68ce380634f657e631a
liumeng0403 <liumeng0403@163.com>
2018-11-06 16:13:55 +0800

    [add] quick api test case

--- MyDAL.Test.QuickAPI/06-ListAsync.cs
+++ MyDAL.Test.QuickAPI/06-ListAsync.cs
@@ -8,16 +8,50 @@ using Yunyong.DataExchange;
 
 namespace MyDAL.Test.QuickAPI
 {
-    public class _06_ListAsync:TestBase
+    public class _06_ListAsync : TestBase
     {
         [Fact]
         public async Task test()
         {
+            /****************************************************************************************/
+
+            var xx1 = "";
+
+            var date = DateTime.Parse("2018-08-20");
+            var res1 = await Conn.ListAsync<AlipayPaymentRecord>(it => it.CreatedOn >= date);
+            Assert.True(res1.Count == 29);
+
+            var tuple1 = (XDebug.SQL, XDebug.Parameters, XDebug.SqlWithParams);
+
+            /****************************************************************************************/
+
+            var xx2 = "";
+
+            var res2 = await Conn.ListAsync<AlipayPaymentRecord, AlipayPaymentRecordVM>(it => it.CreatedOn >= date);
+            Assert.True(res1.Count == 29);
+
+            var tuple2 = (XDebug.SQL, XDebug.Parameters, XDebug.SqlWithParams);
+
+            /****************************************************************************************/
+
+            var xx3 = "";
+
+            var res3 = await Conn.ListAsync<AlipayPaymentRecord, AlipayPaymentRecordVM>(it => it.CreatedOn >= date,
+                it => new AlipayPaymentRecordVM
+                {
+                    TotalAmount = it.TotalAmount,
+                    Description = it.Description
+                });
+            Assert.True(res3.Count == 29);
+
+            var tuple3 = (XDebug.SQL, XDebug.Parameters, XDebug.SqlWithParams);
+
+            /****************************************************************************************/
 
             var xx4 = "";
 
             var option4 = new AlipayPaymentQueryOption();
-            option4.StartTime = DateTime.Parse("2018-08-20");
+            option4.StartTime = date;
             var res4 = await Conn.ListAsync<AlipayPaymentRecord>(option4);
             Assert.True(res4.Count == 29);
 
@@ -36,11 +70,12 @@ namespace MyDAL.Test.QuickAPI
 
             var xx6 = "";
 
-            var res6 = await Conn.ListAsync<AlipayPaymentRecord, AlipayPaymentRecordVM>(option4, record => new AlipayPaymentRecordVM
-            {
-                TotalAmount = record.TotalAmount,
-                Description = record.Description
-            });
+            var res6 = await Conn.ListAsync<AlipayPaymentRecord, AlipayPaymentRecordVM>(option4,
+                record => new AlipayPaymentRecordVM
+                {
+                    TotalAmount = record.TotalAmount,
+                    Description = record.Description
+                });
             Assert.True(res4.Count == 29);
 
             var tuple6 = (XDebug.SQL, XDebug.Parameters, XDebug.SqlWithParams);
