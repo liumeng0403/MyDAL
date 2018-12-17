c93b8294e36f91b091b6f4f076578d4e8decb544
liumeng0403 <liumeng0403@163.com>
2018-12-17 13:04:23 +0800

    [add] more test case

---
+++ MyDAL.Test.NetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyDAL.Test.NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
