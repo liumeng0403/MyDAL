9f2c35784cd2009bf16f14746d3c5e78231a9c78
liumeng0403 <liumeng0403@163.com>
2019-04-10 21:54:43 +0800

    [add]  bug-fix col-prop

--- MyDAL.Test/Entities/MyDAL_TestDB/BodyFitRecord.cs
+++ MyDAL.Test/Entities/MyDAL_TestDB/BodyFitRecord.cs
@@ -19,6 +19,7 @@ namespace MyDAL.Test.Entities.MyDAL_TestDB
     public class BodyFitRecord
     {
         public Guid Id { get; set; }
+        [XColumn(Name = "CreatedOn_col")]
         public DateTime CreatedOn { get; set; }
         public Guid UserId { get; set; }
         public string BodyMeasureProperty { get; set; }
