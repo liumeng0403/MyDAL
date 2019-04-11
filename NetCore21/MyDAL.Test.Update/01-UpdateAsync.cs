981fe7bc024ca4f52efd5b3de85bbacc343a912e
liumeng0403 <liumeng0403@163.com>
2019-04-11 20:14:35 +0800

    [add]  update 注释

--- NetCore21/MyDAL.Test.Update/01-UpdateAsync.cs
+++ NetCore21/MyDAL.Test.Update/01-UpdateAsync.cs
@@ -192,11 +192,45 @@ namespace MyDAL.Test.Update
             var pk1 = Guid.Parse("8f2cbb64-8356-4482-88ee-016558c05b2d");
 
             // 修改一条数据: AlipayPaymentRecord
-            var res1 = await Conn.UpdateAsync<AlipayPaymentRecord>(it => it.Id == pk1,     //  where 条件: it=>it.Id==pk1
+            var res1 = await Conn.UpdateAsync<AlipayPaymentRecord>(it => it.Id == pk1,     //  where 条件: it=>it.Id==pk1 , 可输入任意条件的表达式
             new
             {
                 Description = "new desc",    // 修改 AlipayPaymentRecord 字段 Description 的值为: "new desc"
                 PaymentUrl = "new url"    //  修改 AlipayPaymentRecord 字段 PaymentUrl 的值为: "new url"
+            });
+
+            Assert.True(res1 == 1);
+
+            tuple = (XDebug.SQL, XDebug.Parameters, XDebug.SqlWithParams);
+
+            // 查询一条数据: AlipayPaymentRecord
+            var res11 = await Conn.QueryOneAsync<AlipayPaymentRecord>(it => it.Id == pk1);
+            Assert.True(res11.Description == "new desc");
+
+            /****************************************************************************************/
+
+            xx = string.Empty;
+
+        }
+
+        [Fact]
+        public async Task Update_Shortcut_02()
+        {
+
+            xx = string.Empty;
+
+            var pk1 = Guid.Parse("8f2cbb64-8356-4482-88ee-016558c05b2d");
+
+            var model = new AlipayPaymentRecord();
+            model.Description = "new desc";   // 修改 AlipayPaymentRecord 字段 Description 的值为: "new desc"
+            model.PaymentUrl = "new url";  //  修改 AlipayPaymentRecord 字段 PaymentUrl 的值为: "new url"
+
+            // 修改一条数据: AlipayPaymentRecord
+            var res1 = await Conn.UpdateAsync<AlipayPaymentRecord>(it => it.Id == pk1,  // where 条件: it=>it.Id==pk1 , 可输入任意条件的表达式
+            new
+            {
+                model.Description,    // 修改 AlipayPaymentRecord 字段 Description 的值
+                model.PaymentUrl    //  修改 AlipayPaymentRecord 字段 PaymentUrl 的值
             });
 
             Assert.True(res1 == 1);
