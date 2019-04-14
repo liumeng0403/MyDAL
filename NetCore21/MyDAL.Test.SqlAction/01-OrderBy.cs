85ba266f07cc89d4e75e5edf33d6cbfa87f7c382
liumeng0403 <liumeng0403@163.com>
2019-04-14 23:57:28 +0800

    [add]  test case action

---
+++ NetCore21/MyDAL.Test.SqlAction/01-OrderBy.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace MyDAL.Test.SqlAction
+{
+    public class _01_OrderBy
+        : TestBase
+    {
+        [Fact]
+        public async Task Where_OrderBy_QueryPaging_ST()
+        {
+
+        }
+
+        [Fact]
+        public async Task Where_OrderBy_ThenOrderBy_QueryPaging_ST()
+        {
+
+        }
+
+        [Fact]
+        public async Task Where_OrderBy_QueryPaging_MT()
+        {
+
+        }
+
+        [Fact]
+        public async Task Where_OrderBy_ThenOrderBy_QueryPaging_MT()
+        {
+
+        }
+    }
+}
