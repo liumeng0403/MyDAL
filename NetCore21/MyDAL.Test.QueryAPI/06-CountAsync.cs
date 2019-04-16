bfcfa17540c70558c9233777d0d6fccc9ee347d9
liumeng0403 <liumeng0403@163.com>
2019-04-16 23:54:56 +0800

    [add] test case 整理，移除 test.shortcut project

---
+++ NetCore21/MyDAL.Test.QueryAPI/06-CountAsync.cs
@@ -0,0 +1,55 @@
+﻿using HPC.DAL;
+using MyDAL.Test.Entities.MyDAL_TestDB;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyDAL.Test.QueryAPI
+{
+    public class _06_CountAsync
+        : TestBase
+    {
+        [Fact]
+        public async Task Count_Star_Shortcut()
+        {
+            xx = string.Empty;
+
+            var res1 = await Conn.CountAsync<Agent>(it => it.Name.Length > 3);
+
+            Assert.True(res1 == 116);
+
+            tuple = (XDebug.SQL, XDebug.Parameters, XDebug.SqlWithParams);
+
+            xx = string.Empty;
+        }
+
+        [Fact]
+        public async Task Count_SpecialColumn_Shortcut()
+        {
+
+        }
+
+        [Fact]
+        public async Task Count_Star_ST()
+        {
+
+        }
+
+        [Fact]
+        public async Task Count_SpecialColumn_ST()
+        {
+
+        }
+
+        [Fact]
+        public async Task Count_Star_MT()
+        {
+
+        }
+
+        [Fact]
+        public async Task Count_SpecialColumn_MT()
+        {
+
+        }
+    }
+}
