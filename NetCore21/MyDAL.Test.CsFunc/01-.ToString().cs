b03bbd766844c36a0232d4edb3d1458c1c398fff
liumeng0403 <liumeng0403@163.com>
2019-04-20 00:08:33 +0800

    [add] plan-01 query one ToString

---
+++ NetCore21/MyDAL.Test.CsFunc/01-.ToString().cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace MyDAL.Test.CsFunc
+{
+    public class _01_
+        : TestBase
+    {
+        [Fact]
+        public async Task QueryOne_SingleColumn()
+        {
+
+        }
+
+        [Fact]
+        public async Task QueryOne_VmColumn()
+        {
+
+        }
+    }
+}
