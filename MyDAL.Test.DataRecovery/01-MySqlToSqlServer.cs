d8ec0ab46235104cc07d865d0059b7990df90758
liumeng0403 <liumeng0403@163.com>
2019-04-21 21:13:44 +0800

    [add] data recovery test

--- MyDAL.Test.DataRecovery/01-MySqlToSqlServer.cs
+++ MyDAL.Test.DataRecovery/01-MySqlToSqlServer.cs
@@ -4,9 +4,9 @@ using System;
 namespace MyDAL.Test.DataRecovery
 {
     public class _01_MySqlToSqlServer
-        :TestBase
+        : TestBase
     {
-        
+
         public void MySqlToSqlServer2012SP1Plus()
         {
 
@@ -16,11 +16,11 @@ namespace MyDAL.Test.DataRecovery
              * AddressInfo
              */
 
-            var sqlDelete01 = Conn2.DeleteAsync<AddressInfo>(null).GetAwaiter().GetResult();
+            var sqlDelete01 = Conn2.Delete<AddressInfo>(it => true);
             PrintLog($"清理 sql server AddressInfo 表：{sqlDelete01}");
-            var dataList01 = Conn.QueryListAsync<AddressInfo>(null).GetAwaiter().GetResult();
+            var dataList01 = Conn.QueryList<AddressInfo>(it => true);
             PrintLog($"查询 my sql AddressInfo 表：{dataList01.Count}");
-            var sqlCreate01 = Conn2.CreateBatchAsync(dataList01).GetAwaiter().GetResult();
+            var sqlCreate01 = Conn2.CreateBatch(dataList01);
             PrintLog($"恢复 sql server AddressInfo 表：{sqlCreate01}");
 
             PrintLog("--------------------------------------------------------------------------------------");
@@ -29,11 +29,11 @@ namespace MyDAL.Test.DataRecovery
              * AlipayPaymentRecord
              */
 
-            var sqlDelete02 = Conn2.DeleteAsync<AlipayPaymentRecord>(null).GetAwaiter().GetResult();
+            var sqlDelete02 = Conn2.Delete<AlipayPaymentRecord>(it => true);
             PrintLog($"清理 sql server AlipayPaymentRecord 表：{sqlDelete02}");
-            var dataList02 = Conn.QueryListAsync<AlipayPaymentRecord>(null).GetAwaiter().GetResult();
+            var dataList02 = Conn.QueryList<AlipayPaymentRecord>(it => true);
             PrintLog($"查询 my sql AlipayPaymentRecord 表：{dataList02.Count}");
-            var sqlCreate02 = Conn2.CreateBatchAsync(dataList02).GetAwaiter().GetResult();
+            var sqlCreate02 = Conn2.CreateBatch(dataList02);
             PrintLog($"恢复 sql server AlipayPaymentRecord 表：{sqlCreate02}");
 
             PrintLog("--------------------------------------------------------------------------------------");
@@ -42,13 +42,13 @@ namespace MyDAL.Test.DataRecovery
              * Agent
              */
 
-            var sqlDelete03 = Conn2.DeleteAsync<Agent>(null).GetAwaiter().GetResult();
+            var sqlDelete03 = Conn2.Delete<Agent>(it => true);
             PrintLog($"清理 sql server Agent 表：{sqlDelete03}");
-            var dataList03 = Conn.QueryListAsync<Agent>(null).GetAwaiter().GetResult();
+            var dataList03 = Conn.QueryList<Agent>(it => true);
             PrintLog($"查询 my sql Agent 表：{dataList03.Count}");
-            var total03 = StepProcess<Agent>(dataList03, 1000, list =>
+            var total03 = StepProcess<Agent>(dataList03, 300, list =>
             {
-                var num = Conn2.CreateBatchAsync(list).GetAwaiter().GetResult();
+                var num = Conn2.CreateBatch(list);
                 PrintLog($"恢复 sql server Agent 表：{num}");
                 return num;
             });
@@ -60,11 +60,11 @@ namespace MyDAL.Test.DataRecovery
              * AgentInventoryRecord
              */
 
-            var sqlDelete04 = Conn2.DeleteAsync<AgentInventoryRecord>(null).GetAwaiter().GetResult();
+            var sqlDelete04 = Conn2.Delete<AgentInventoryRecord>(it => true);
             PrintLog($"清理 sql server AgentInventoryRecord 表：{sqlDelete04}");
-            var dataList04 = Conn.QueryListAsync<AgentInventoryRecord>(null).GetAwaiter().GetResult();
+            var dataList04 = Conn.QueryList<AgentInventoryRecord>(it => true);
             PrintLog($"查询 my sql AgentInventoryRecord 表：{dataList04.Count}");
-            var sqlCreate04 = Conn2.CreateBatchAsync(dataList04).GetAwaiter().GetResult();
+            var sqlCreate04 = Conn2.CreateBatch(dataList04);
             PrintLog($"恢复 sql server AgentInventoryRecord 表：{sqlCreate04}");
 
             PrintLog("--------------------------------------------------------------------------------------结束");
