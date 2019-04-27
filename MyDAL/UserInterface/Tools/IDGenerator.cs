e602549adf8f5639973f510c3965d23b443bc0b1
liumeng0403 <liumeng0403@163.com>
2019-04-27 20:13:44 +0800

    [add] long id snowflake

---
+++ MyDAL/UserInterface/Tools/IDGenerator.cs
@@ -0,0 +1,18 @@
+﻿using HPC.DAL.Core.Common.Tools;
+
+namespace HPC.DAL.Tools
+{
+    public class IDGenerator
+    {
+        /// <summary>
+        /// Twitter SnowFlake ID
+        /// </summary>
+        public long LongID
+        {
+            get
+            {
+                return new SnowFlake().GetSerialID();
+            }
+        }
+    }
+}
---
+++ MyDAL/UserInterface/Tools/StringEx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HPC.DAL.ModelTools
+{
+    /// <summary>
+    /// 请参阅: <see langword="目录索引 https://www.cnblogs.com/Meng-NET/"/>
+    /// </summary>
+    public static class StringEx
+    {
+
+        /// <summary>
+        /// Is null/empty/whitespace ?
+        /// </summary>
+        [Obsolete("警告：建议使用 Tools 命名空间中此方法， 此 API 后面会从 ModelTools 中移除！！！", false)]
+        public static bool IsNullStr(this string str)
+        {
+            return string.IsNullOrWhiteSpace(str);
+        }
+
+    }
+}
+namespace HPC.DAL.Tools
+{
+    /// <summary>
+    /// 请参阅: <see langword="目录索引 https://www.cnblogs.com/Meng-NET/"/>
+    /// </summary>
+    public static class StringEx
+    {
+
+        /// <summary>
+        /// Is null/empty/whitespace ?
+        /// </summary>
+        public static bool IsNullStr(this string str)
+        {
+            return string.IsNullOrWhiteSpace(str);
+        }
+
+    }
+}
