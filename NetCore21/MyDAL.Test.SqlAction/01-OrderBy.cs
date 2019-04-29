9e225ce2d9fc0ce148fddf5e3208ead1fdf3e060
liumeng0403 <liumeng0403@163.com>
2019-04-29 10:36:55 +0800

    [add] order by api 完善

--- NetCore21/MyDAL.Test.SqlAction/01-OrderBy.cs
+++ NetCore21/MyDAL.Test.SqlAction/01-OrderBy.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using MyDAL.Test.Entities.MyDAL_TestDB;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace MyDAL.Test.SqlAction
@@ -7,9 +8,48 @@ namespace MyDAL.Test.SqlAction
         : TestBase
     {
         [Fact]
-        public async Task Where_OrderBy_QueryPaging_ST()
+        public async Task Queryer_OrderBy_QueryPaging_ST()
+        {
+            xx = string.Empty;
+
+            var res1 = await Conn
+                .Queryer<Agent>()
+                .OrderBy(it => it.Name, OrderByEnum.Desc)
+                .QueryPagingAsync(1, 10, it => it.Name);
+
+            Assert.True(res1.Data.Count == 10);
+
+            tuple = (XDebug.SQL, XDebug.Parameters, XDebug.SqlWithParams);
+
+            /*****************************************************************************************************************************/
+
+            xx = string.Empty;
+        }
+
+        [Fact]
+        public async Task Queryer_OrderBy_ThenOrderBy_QueryPaging_ST()
         {
 
+        }
+
+        [Fact]
+        public async Task Where_OrderBy_QueryPaging_ST()
+        {
+            xx = string.Empty;
+
+            var res1 = await Conn
+                .Queryer<Agent>()
+                .Where(it => it.Name.StartsWith("张"))
+                .OrderBy(it => it.Name, OrderByEnum.Desc)
+                .QueryPagingAsync(1, 10, it => it.Name);
+
+            Assert.True(res1.Data.Count == 10);
+
+            tuple = (XDebug.SQL, XDebug.Parameters, XDebug.SqlWithParams);
+
+            /*****************************************************************************************************************************/
+
+            xx = string.Empty;
         }
 
         [Fact]
