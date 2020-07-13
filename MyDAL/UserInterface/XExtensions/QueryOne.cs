07440e2472d11bea79a0bc2710796386e1425ee1
liumeng <liumeng0403@163.com>
2020-07-13 22:49:42 +0800

    query one / query list net4 分离

---
+++ MyDAL/UserInterface/XExtensions/QueryOne.cs
@@ -0,0 +1,243 @@
+﻿using MyDAL.Impls.ImplAsyncs;
+using MyDAL.Impls.ImplSyncs;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MyDAL
+{
+    /// <summary>
+    /// 请参阅: <see langword="目录索引 https://www.cnblogs.com/Meng-NET/"/>
+    /// </summary>
+    public static partial class XExtension
+    {
+
+        #region QueryOne API
+
+
+        /*-------------------------------------------------------------*/
+
+
+        /*-------------------------------------------------------------*/
+
+
+        /*-------------------------------------------------------------*/
+
+        public static bool QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, bool>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static bool? QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, bool?>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static byte QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, byte>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static byte? QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, byte?>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static char QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, char>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static char? QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, char?>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static decimal QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, decimal>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static decimal? QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, decimal?>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static double QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, double>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static double? QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, double?>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static float QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, float>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static float? QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, float?>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static int QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, int>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static int? QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, int?>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static long QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, long>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static long? QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, long?>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static sbyte QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, sbyte>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static sbyte? QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, sbyte?>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static short QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, short>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static short? QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, short?>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static uint QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, uint>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static uint? QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, uint?>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static ulong QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, ulong>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static ulong? QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, ulong?>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static ushort QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, ushort>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static ushort? QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, ushort?>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static DateTime QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, DateTime>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static DateTime? QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, DateTime?>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static TimeSpan QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, TimeSpan>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static TimeSpan? QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, TimeSpan?>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static Guid QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, Guid>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static Guid? QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, Guid?>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static object QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, object>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static byte[] QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, byte[]>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+        public static string QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, string>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+
+        /*-------------------------------------------------------------*/
+
+        /// <summary>
+        /// Queryer 便捷-同步 QueryOneAsync 方法
+        /// </summary>
+        public static M QueryOne<M>(this XConnection conn, Expression<Func<M, bool>> compareFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne();
+        }
+        /// <summary>
+        /// Queryer 便捷-同步 QueryOneAsync 方法
+        /// </summary>
+        public static VM QueryOne<M, VM>(this XConnection conn, Expression<Func<M, bool>> compareFunc)
+            where M : class, new()
+            where VM : class
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne<VM>();
+        }
+        /// <summary>
+        /// Queryer 便捷-同步 QueryOneAsync 方法
+        /// </summary>
+        public static T QueryOne<M, T>(this XConnection conn, Expression<Func<M, bool>> compareFunc, Expression<Func<M, T>> columnMapFunc)
+            where M : class, new()
+        {
+            return conn.Queryer<M>().Where(compareFunc).QueryOne(columnMapFunc);
+        }
+
+        /*-------------------------------------------------------------*/
+
+        public static T QueryOne<T>(this XConnection conn, string sql, List<XParam> dbParas = null)
+        {
+            CheckQuery(sql);
+            var dc = DcForSQL(conn, sql, dbParas);
+            return new QueryOneSQLImpl(dc).QueryOne<T>();
+        }
+
+        #endregion
+
+    }
+}
--- MyDAL/UserInterface/XExtensions/Update.cs
+++ MyDAL/UserInterface/XExtensions/Update.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
-using System.Threading.Tasks;
 
 namespace MyDAL
 {
@@ -13,23 +12,9 @@ namespace MyDAL
 
         #region Update API
 
-        /// <summary>
-        /// 请参阅: <see langword=".UpdateAsync() 使用 https://www.cnblogs.com/Meng-NET/"/>
-        /// </summary>
-        public static async Task<int> UpdateAsync<M>
-            (this XConnection conn, Expression<Func<M, bool>> compareFunc, dynamic filedsObject)
-            where M : class, new()
-        {
-            return await conn.Updater<M>().Set(filedsObject as object).Where(compareFunc).UpdateAsync();
-        }
 
         /*-------------------------------------------------------------*/
 
-        public static async Task<int> UpdateAsync(this XConnection conn, string sql, List<XParam> dbParas = null)
-        {
-            CheckUpdate(sql);
-            return await conn.ExecuteNonQueryAsync(sql, dbParas);
-        }
 
         /*-------------------------------------------------------------*/
 
