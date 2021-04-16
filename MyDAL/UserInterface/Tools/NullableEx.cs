ad74de2c05453704e3e8a3d14f313496281efcbd
liumeng <liumeng0403@163.com>
2021-04-17 00:19:26 +0800

    V0.9.1.15 增加可空判断 四个实用函数

--- MyDAL/UserInterface/Tools/NullableEx.cs
+++ MyDAL/UserInterface/Tools/NullableEx.cs
@@ -18,5 +18,39 @@
         {
             return !nullableBool.IsNull();
         }
+
+        public static bool IsNull(this int? nullableInt)
+        {
+            if (null == nullableInt)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsNotNull(this int? nullableInt)
+        {
+            return !nullableInt.IsNull();
+        }
+
+        public static bool IsNull(this long? nullableLong)
+        {
+            if (null == nullableLong)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsNotNull(this long? nullableLong)
+        {
+            return !nullableLong.IsNull();
+        }
     }
 }
