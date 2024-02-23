f73fb97b1370c66b4339e1dd833a2bd1ed06d991
220551659 <liumeng0403@163.com>
2024-02-23 22:17:59 +0800

    sum 函数 part01

---
+++ Function_Count/Sum.cs
@@ -0,0 +1,14 @@
+using Mysql.DataNet6;
+
+namespace Function_Count;
+
+[TestClass]
+public class Sum
+    : Net6Base
+{
+    [TestMethod]
+    public void xxx()
+    {
+        
+    }
+}
---
+++ MyDAL/UserInterface/Functions/SUM.cs
@@ -0,0 +1,36 @@
+namespace MyDAL
+{
+    /// <summary>
+    /// MySQL 函数
+    /// </summary>
+    public sealed partial class XFunction
+    {
+    
+        public static decimal SUM(decimal field) { return 0; }
+        public static decimal? SUM(decimal? field) { return 0; }
+        
+        public static double SUM(double field) { return 0; }
+        public static double? SUM(double? field) { return 0; }
+        
+        public static float SUM(float field) { return 0; }
+        public static float? SUM(float? field) { return 0; }
+        
+        public static long SUM(int field) { return 0; }
+        public static long? SUM(int? field) { return 0; }
+        
+        public static long SUM(long field) { return 0; }
+        public static long? SUM(long? field) { return 0; }
+
+        public static long SUM(short field) { return 0; }
+        public static long? SUM(short? field) { return 0; }
+        
+        public static long SUM(uint field) { return 0; }
+        public static long? SUM(uint? field) { return 0; }
+        
+        public static long SUM(ushort field) { return 0; }
+        public static long? SUM(ushort? field) { return 0; }
+       
+    
+        
+    }
+}
